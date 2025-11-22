20e2c0eea2e4972bc2968e84e9ee0b277266b63d
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-11-22 08:55:35 +0100

    Update ontoportal.py

--- src/sssom_pydantic/contrib/ontoportal.py
+++ src/sssom_pydantic/contrib/ontoportal.py
@@ -7,7 +7,7 @@ from typing import TYPE_CHECKING, Any
 
 import curies
 from curies import Reference
-from curies.vocabulary import has_dbxref, lexical_matching_process
+from curies.vocabulary import exact_match, lexical_matching_process
 
 from sssom_pydantic import MappingTool, SemanticMapping
 
@@ -18,11 +18,34 @@ logger = logging.getLogger(__name__)
 
 
 def from_bioportal(
-    o1: str, o2: str, *, converter: curies.Converter | None = None
+    o1: str,
+    o2: str,
+    *,
+    converter: curies.Converter | None = None,
+    client: ontoportal_client.BioPortalClient | None = None,
 ) -> list[SemanticMapping]:
-    from ontoportal_client import BioPortalClient
+    """Get mappings from BioPortal.
 
-    return from_ontoportal(o1, o2, client=BioPortalClient(), converter=converter)
+    :param o1: The first ontology
+    :param o2: The second ontology
+    :param client: A pre-instantiated BioPortal client. If not given, will try to
+        automatically construct one. Note that this requires having an API key
+        configured.
+    :param converter: A converter for parsing URIs
+
+    :returns: A list of semantic mappings.
+
+    .. warning::
+
+        BioPortal contains irrelevant mappings, i.e., ones that are made between
+        imported terms. Therefore, you should check that the ontologies match the parsed
+        results
+    """
+    if client is None:
+        from ontoportal_client import BioPortalClient
+
+        client = BioPortalClient()
+    return from_ontoportal(o1, o2, client=client, converter=converter)
 
 
 def from_ontoportal(
@@ -32,6 +55,25 @@ def from_ontoportal(
     converter: curies.Converter | None = None,
     client: ontoportal_client.Client,
 ) -> list[SemanticMapping]:
+    """Get mappings from an OntoPortal instance.
+
+    :param o1: The first ontology
+    :param o2: The second ontology
+    :param client: A pre-instantiated OntoPortal client, e.g., to BioPortal, AgroPortal,
+        EcoPortal, etc.
+    :param converter: A converter for parsing URIs
+
+    :returns: A list of semantic mappings.
+
+    .. warning::
+
+        OntoPortal's mapping data model includes any mappings between terms that have
+        been imported into the ontologies, meaning that you should filter post-facto to
+        only keep mappings whose subject/object match the query ontologies.
+
+        This isn't possible to do directly because OntoPortal's data model does not
+        contain a prefix map nor information about the defining ontology for a term
+    """
     if converter is None:
         import bioregistry
 
@@ -58,12 +100,13 @@ def _process(data: dict[str, Any], converter: curies.Converter) -> SemanticMappi
         # see https://www.bioontology.org/wiki/LOOM
         mapping_tool = MappingTool(name="LOOM")
         justification = lexical_matching_process
+        predicate = exact_match
     else:
         logger.warning("unhandled mapping tool: %s", tool)
         return None
     return SemanticMapping(
         subject=subject,
-        predicate=has_dbxref,
+        predicate=predicate,
         object=obj,
         justification=justification,
         mapping_tool=mapping_tool,
@@ -77,7 +120,3 @@ def _process_class(data: dict[str, Any], converter: curies.Converter) -> Referen
         logger.warning("could not parse: %s", uri)
         return None
     return reference_tuple.to_pydantic()
-
-
-if __name__ == "__main__":
-    from_bioportal("SNOMEDCT", "AERO")
