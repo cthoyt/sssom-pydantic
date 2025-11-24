c4fc694a46bb2a8071dc89f176b5f9a7bfd4c8e9
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-11-24 12:22:28 +0100

    Update tests

--- tests/test_contrib/test_ontoportal.py
+++ tests/test_contrib/test_ontoportal.py
@@ -6,19 +6,21 @@ from pathlib import Path
 
 import curies
 from curies import Reference
-from curies.vocabulary import exact_match, lexical_matching_process
+from curies.vocabulary import exact_match, lexical_matching_process, mapping_chaining
 
 from sssom_pydantic import MappingTool, SemanticMapping
 from sssom_pydantic.contrib.ontoportal import _process
 
 HERE = Path(__file__).parent.resolve()
-PATH = HERE.joinpath("ontoportal_example.json")
+LOOM_EXAMPLE_PATH = HERE.joinpath("ontoportal_loom_example.json")
+CUI_EXAMPLE_PATH = HERE.joinpath("ontoportal_cui_example.json")
+SAME_URI_EXAMPLE_PATH = HERE.joinpath("ontoportal_same_uri_example.json")
 
 
 class TestBioportal(unittest.TestCase):
     """Test processing OntoPortal mappings."""
 
-    def test_process(self) -> None:
+    def test_process_loom(self) -> None:
         """Test processing OntoPortal mappings."""
         converter = curies.Converter.from_prefix_map(
             {
@@ -26,7 +28,7 @@ class TestBioportal(unittest.TestCase):
                 "SNOMEDCT": "http://purl.bioontology.org/ontology/SNOMEDCT/",
             }
         )
-        data = json.loads(PATH.read_text())
+        data = json.loads(LOOM_EXAMPLE_PATH.read_text())
         mapping = _process(data, converter)
         self.assertIsNotNone(mapping)
         expected = SemanticMapping(
@@ -37,3 +39,30 @@ class TestBioportal(unittest.TestCase):
             mapping_tool=MappingTool(name="LOOM"),
         )
         self.assertEqual(expected, mapping)
+
+    def test_process_cui(self) -> None:
+        """Test processing OntoPortal mappings."""
+        converter = curies.Converter.from_prefix_map(
+            {
+                "ATC": "http://purl.bioontology.org/ontology/ATC/",
+                "SNOMEDCT": "http://purl.bioontology.org/ontology/SNOMEDCT/",
+            }
+        )
+        data = json.loads(CUI_EXAMPLE_PATH.read_text())
+        mapping = _process(data, converter)
+        self.assertIsNotNone(mapping)
+        expected = SemanticMapping(
+            object=Reference(prefix="ATC", identifier="N05AX15"),
+            predicate=exact_match,
+            subject=Reference(prefix="SNOMEDCT", identifier="715295006"),
+            justification=mapping_chaining,
+            mapping_tool=None,
+        )
+        self.assertEqual(expected, mapping)
+
+    def test_process_same_uri(self) -> None:
+        """Test processing OntoPortal mappings."""
+        converter = curies.Converter()
+        data = json.loads(SAME_URI_EXAMPLE_PATH.read_text())
+        mapping = _process(data, converter)
+        self.assertIsNone(mapping)
