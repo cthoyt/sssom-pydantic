8b81643481112858289c6c38ca7e2840f94de80b
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-12-11 23:26:13 +0100

    Update test_fastapi.py

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -1,8 +1,10 @@
 """Test mapping API."""
 
+import tempfile
 import unittest
+from pathlib import Path
 
-from curies import Reference
+from pydantic import BaseModel
 from starlette.testclient import TestClient
 
 from sssom_pydantic import SemanticMapping
@@ -15,14 +17,35 @@ from tests.cases import _m
 class TestFastAPI(unittest.TestCase):
     """Test API."""
 
+    def assert_model_equal(self, expected: BaseModel, actual: BaseModel) -> None:
+        """Assert that two models are equal."""
+        self.assertEqual(
+            expected.model_dump(exclude_unset=True, exclude_none=True),
+            actual.model_dump(exclude_unset=True, exclude_none=True),
+        )
+
     def test_api(self) -> None:
         """Test the components of a mapping API."""
-        input_mapping = _m(record=Reference(prefix="sssom", identifier="123456"))
-        database = SemanticMappingDatabase.memory(semantic_mapping_hash=mapping_hash_v1)
-        database.add_mapping(input_mapping)
-        app = get_app(database=database)
-        client = TestClient(app)
+        expected = _m()
 
-        response = client.get("/mapping/sssom:123456")
-        s = SemanticMapping.model_validate(response.json())
-        self.assertEqual(input_mapping, s)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            # this can't be memory because of weird thread-safe stuff
+            path = Path(tmpdir).joinpath("test.db")
+
+            database = SemanticMappingDatabase.from_connection(
+                connection=f"sqlite:///{path}",
+                semantic_mapping_hash=mapping_hash_v1,
+            )
+            database.add_mapping(expected)
+
+            reference = database._hsh(expected)
+            self.assertIsNotNone(database.get_mapping(reference))
+
+            app = get_app(database=database)
+            client = TestClient(app)
+
+            response = client.get(f"/mapping/{reference.curie}")
+            response.raise_for_status()
+            response_json = response.json()
+            actual = SemanticMapping.model_validate(response_json)
+            self.assert_model_equal(expected, actual)
