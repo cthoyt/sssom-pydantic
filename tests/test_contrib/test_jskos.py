8408a5932eed9c3d201702b0a1000c543cbf1a99
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-12-11 21:57:49 +0100

    Update test_jskos.py

--- tests/test_contrib/test_jskos.py
+++ tests/test_contrib/test_jskos.py
@@ -1,13 +1,19 @@
 """Test JSKOS export."""
 
 import importlib.util
+import subprocess
+import tempfile
 import unittest
+from pathlib import Path
 
 from curies import Converter, Reference
 from curies.vocabulary import exact_match, manual_mapping_curation
 from pydantic import BaseModel
 
-from sssom_pydantic import MappingSet, SemanticMapping
+import sssom_pydantic
+from sssom_pydantic import SemanticMapping
+from sssom_pydantic.examples import EXAMPLE_MAPPINGS
+from tests.cases import TEST_CONVERTER, TEST_METADATA
 
 
 @unittest.skipUnless(importlib.util.find_spec("jskos"), reason="requires JSKOS")
@@ -34,18 +40,16 @@ class TestJSKOSExport(unittest.TestCase):
                 "semapv": "https://w3id.org/semapv/vocab/",
             }
         )
-        mapping_set_id = "https://example.org/mappings"
         license_uri = "https://creativecommons.org/licenses/by/4.0/"
-        mapping_set = MappingSet(id=mapping_set_id)
         mapping = SemanticMapping(
             subject=Reference(prefix="x", identifier="1"),
             predicate=exact_match,
             object=Reference(prefix="x", identifier="1"),
             justification=manual_mapping_curation,
         )
-        d = {
+        expected_jskos_record = {
             "license": [{"uri": license_uri}],
-            "uri": mapping_set_id,
+            "uri": TEST_METADATA.mapping_set_id,
             "mappings": [
                 {
                     "type": ["http://www.w3.org/2004/02/skos/core#exactMatch"],
@@ -55,5 +59,36 @@ class TestJSKOSExport(unittest.TestCase):
                 }
             ],
         }
-        expected = Concept.model_validate(d)
-        self.assert_model_equal(expected, mapping_set_to_jskos([mapping], converter, mapping_set))
+        expected = Concept.model_validate(expected_jskos_record)
+        self.assert_model_equal(
+            expected, mapping_set_to_jskos([mapping], converter, TEST_METADATA.process(converter))
+        )
+
+    def test_all_jskos(self) -> None:
+        """Test converting examples to JSKOS then back."""
+        import jskos
+
+        for i, example in enumerate(EXAMPLE_MAPPINGS):
+            with self.subTest(i=i), tempfile.TemporaryDirectory() as td:
+                tsv_path = Path(td).joinpath("example.sssom.tsv")
+                sssom_pydantic.write(
+                    [example], tsv_path, metadata=TEST_METADATA, converter=TEST_CONVERTER
+                )
+
+                # Convert the SSSOM TSV to JSKOS using the sssom-js package
+                # on NPM (https://www.npmjs.com/package/sssom-js)
+                jskos_path = Path(td).joinpath("example.sssom.json")
+                subprocess.call(  # noqa:S603
+                    [  # noqa:S607
+                        "npx",
+                        "sssom-js",
+                        "--from",
+                        "csv",
+                        "--to",
+                        "jskos",
+                        "--output",
+                        jskos_path.as_posix(),
+                        tsv_path.as_posix(),
+                    ]
+                )
+                jskos.read(jskos_path)
