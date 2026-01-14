a85f09c4c8aaacf70892dc07d41d0a41b6984714
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-01-14 13:34:26 +0100

    Reduce

--- src/sssom_pydantic/contrib/jskos_export.py
+++ src/sssom_pydantic/contrib/jskos_export.py
@@ -2,13 +2,11 @@
 
 import subprocess
 import tempfile
-from functools import partial
 from pathlib import Path
 from typing import TYPE_CHECKING
 
 import curies
 import jskos
-from jskos import Concept
 
 import sssom_pydantic
 
@@ -18,64 +16,38 @@ if TYPE_CHECKING:
     from sssom_pydantic import MappingSet, MappingSetRecord, Metadata, SemanticMapping
 
 __all__ = [
-    "mapping_set_to_jskos",
-    "mapping_to_jskos",
-    "mapping_to_jskos_oracle",
-    "path_to_jskos_oracle",
+    "to_jskos",
 ]
 
 
-def mapping_set_to_jskos(
-    mappings: list[SemanticMapping], converter: curies.Converter, metadata: MappingSet
-) -> Concept:
-    """Convert a mapping set to JSKOS."""
-    rv = Concept(
-        identifier=[metadata.id],
-        license=[Concept(uri=metadata.license)],
-        mappings=[mapping_to_jskos(mapping, converter) for mapping in mappings],
-    )
-    return rv
-
-
-def mapping_to_jskos(mapping: SemanticMapping, converter: curies.Converter) -> jskos.Mapping:
-    """Convert a mapping to JSKOS."""
-    _r = partial(converter.expand_reference, strict=True)
-
-    return jskos.Mapping.model_validate(
-        {
-            "type": [_r(mapping.predicate)],
-            "from": Concept(member_set=[Concept(uri=_r(mapping.subject))]),
-            "to": Concept(member_set=[Concept(uri=_r(mapping.object))]),
-            "justification": _r(mapping.justification),
-        }
-    )
-
-
-def mapping_to_jskos_oracle(
-    mapping: SemanticMapping,
-    metadata: MappingSet | Metadata | MappingSetRecord | None,
-    converter: curies.Converter,
+def to_jskos(
+    mappings: SemanticMapping | list[SemanticMapping],
+    *,
+    metadata: MappingSet | Metadata | MappingSetRecord | None = None,
+    converter: curies.Converter | None = None,
 ) -> jskos.Concept:
-    """Convert a mapping to JSKOS using sssom-js."""
-    with tempfile.TemporaryDirectory() as td:
-        path = Path(td).joinpath("example.sssom.tsv")
+    """Convert mapping(s) to JSKOS using sssom-js."""
+    if isinstance(mappings, SemanticMapping):
+        mappings = [mappings]
+    with tempfile.TemporaryDirectory() as temporary_directory:
+        path = Path(temporary_directory).joinpath("tmp.sssom.tsv")
         sssom_pydantic.write(
-            [mapping],
+            mappings,
             path,
             metadata=metadata,
             converter=converter,
         )
-        return path_to_jskos_oracle(path)
+        return _path_to_jskos(path)
 
 
-def path_to_jskos_oracle(path: Path) -> jskos.Concept:
+def _path_to_jskos(path: Path) -> jskos.Concept:
     """Convert SSSOM TSV to JSKOS using sssom-js."""
     import jskos
 
-    with tempfile.TemporaryDirectory() as td:
+    with tempfile.TemporaryDirectory() as temporary_directory:
         # Convert the SSSOM TSV to JSKOS using the sssom-js package
         # on NPM (https://www.npmjs.com/package/sssom-js)
-        jskos_path = Path(td).joinpath("example.sssom.json")
+        jskos_path = Path(temporary_directory).joinpath("tmp.sssom.json")
         result = subprocess.run(  # noqa:S603
             [  # noqa:S607
                 "npx",
--- tests/test_contrib/test_jskos.py
+++ tests/test_contrib/test_jskos.py
@@ -2,74 +2,19 @@
 
 import importlib.util
 import unittest
-from typing import TYPE_CHECKING
 
-from curies import Converter, Reference
-from curies.vocabulary import exact_match, manual_mapping_curation
-from pydantic import BaseModel
-
-from sssom_pydantic import SemanticMapping
 from sssom_pydantic.examples import EXAMPLES
 from tests.cases import TEST_CONVERTER, TEST_METADATA
-
-if TYPE_CHECKING:
-    pass
 
 
 @unittest.skipUnless(importlib.util.find_spec("jskos"), reason="requires JSKOS")
 class TestJSKOSExport(unittest.TestCase):
     """Test JSKOS export."""
 
-    def assert_model_equal(self, expected: BaseModel, actual: BaseModel) -> None:
-        """Assert two models are equal."""
-        self.assertEqual(
-            expected.model_dump(exclude_none=True, exclude_unset=True),
-            actual.model_dump(exclude_unset=True, exclude_none=True),
-        )
-
-    def test_jskos(self) -> None:
-        """Test JSKOS export."""
-        from jskos import Concept
-
-        from sssom_pydantic.contrib.jskos_export import mapping_set_to_jskos
-
-        converter = Converter.from_prefix_map(
-            {
-                "skos": "http://www.w3.org/2004/02/skos/core#",
-                "x": "https://example.org/",
-                "semapv": "https://w3id.org/semapv/vocab/",
-            }
-        )
-        license_uri = "https://creativecommons.org/licenses/by/4.0/"
-        mapping = SemanticMapping(
-            subject=Reference(prefix="x", identifier="1"),
-            predicate=exact_match,
-            object=Reference(prefix="x", identifier="1"),
-            justification=manual_mapping_curation,
-        )
-        expected_jskos_record = {
-            "license": [{"uri": license_uri}],
-            "uri": TEST_METADATA.mapping_set_id,
-            "mappings": [
-                {
-                    "type": ["http://www.w3.org/2004/02/skos/core#exactMatch"],
-                    "from": {"memberSet": [{"uri": "http://example.org/1"}]},
-                    "to": {"memberSet": [{"uri": "http://example.org/2"}]},
-                    "justification": "https://w3id.org/semapv/vocab/ManualMappingCuration",
-                }
-            ],
-        }
-        expected = Concept.model_validate(expected_jskos_record)
-        self.assert_model_equal(
-            expected, mapping_set_to_jskos([mapping], converter, TEST_METADATA.process(converter))
-        )
-
-    def test_all_jskos(self) -> None:
-        """Test converting examples to JSKOS then back."""
-        from sssom_pydantic.contrib.jskos_export import mapping_to_jskos_oracle
+    def text_example_output(self) -> None:
+        """Test that all SSSOM examples can be converted to JSKOS."""
+        from sssom_pydantic.contrib.jskos_export import to_jskos
 
         for example in EXAMPLES:
             with self.subTest(desc=example.description):
-                mapping_to_jskos_oracle(
-                    example.semantic_mapping, metadata=TEST_METADATA, converter=TEST_CONVERTER
-                )
+                to_jskos(example.semantic_mapping, converter=TEST_CONVERTER, metadata=TEST_METADATA)
