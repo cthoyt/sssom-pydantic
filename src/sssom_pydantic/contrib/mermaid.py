eeff10161a306c4ca2c8830e3d8467f3c760be29
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-05-28 13:37:28 +0200

    Update mermaid.py

--- src/sssom_pydantic/contrib/mermaid.py
+++ src/sssom_pydantic/contrib/mermaid.py
@@ -6,6 +6,7 @@ from collections.abc import Iterable
 from typing import TYPE_CHECKING
 
 import curies
+from typing_extensions import NotRequired, TypedDict, Unpack
 
 from sssom_pydantic.api import NOT, SemanticMapping, hash_mapping, hash_triple
 
@@ -13,6 +14,7 @@ if TYPE_CHECKING:
     import IPython.display
 
 __all__ = [
+    "MermaidOptions",
     "copy_mermaid_markdown",
     "to_mermaid",
     "to_mermaid_ipython",
@@ -20,36 +22,52 @@ __all__ = [
 ]
 
 
+class MermaidOptions(TypedDict):
+    """Keyword arguments for Mermaid."""
+
+    include_people: NotRequired[bool]
+    include_sources: NotRequired[bool]
+
+
 def to_mermaid_ipython(
-    mappings: Iterable[SemanticMapping], converter: curies.Converter
+    mappings: Iterable[SemanticMapping],
+    converter: curies.Converter,
+    **kwargs: Unpack[MermaidOptions],
 ) -> IPython.display.Markdown:
     """Make a Mermaid IPython display."""
     from IPython.display import Markdown
 
-    return Markdown(to_mermaid_markdown(mappings, converter))
+    return Markdown(to_mermaid_markdown(mappings, converter, **kwargs))
 
 
-def copy_mermaid_markdown(mappings: Iterable[SemanticMapping], converter: curies.Converter) -> None:
+def copy_mermaid_markdown(
+    mappings: Iterable[SemanticMapping],
+    converter: curies.Converter,
+    **kwargs: Unpack[MermaidOptions],
+) -> None:
     """Copy a Mermaid block for Markdown to the clipboard with :mod:`pyperclip`."""
     import pyperclip
 
-    pyperclip.copy(to_mermaid_markdown(mappings, converter))
+    pyperclip.copy(to_mermaid_markdown(mappings, converter, **kwargs))
 
 
-def to_mermaid_markdown(mappings: Iterable[SemanticMapping], converter: curies.Converter) -> str:
+def to_mermaid_markdown(
+    mappings: Iterable[SemanticMapping],
+    converter: curies.Converter,
+    **kwargs: Unpack[MermaidOptions],
+) -> str:
     """Make a Mermaid block for Markdown."""
-    mermaid = to_mermaid(mappings, converter)
+    mermaid = to_mermaid(mappings, converter, **kwargs)
     block = f"```mermaid\n{mermaid}\n```"
     return block
 
 
-def _n(r: curies.Reference) -> str:
-    if name := getattr(r, "name", None):
-        return f"{name}\n{r.curie}"
-    return r.curie
-
-
-def to_mermaid(mappings: Iterable[SemanticMapping], converter: curies.Converter) -> str:
+def to_mermaid(
+    mappings: Iterable[SemanticMapping],
+    converter: curies.Converter,
+    include_people: bool = True,
+    include_sources: bool = True,
+) -> str:
     """Make a mermaid flowchart string."""
     lines = []
     edges = []
@@ -75,19 +93,20 @@ def to_mermaid(mappings: Iterable[SemanticMapping], converter: curies.Converter)
             lines.append(f'{record_id}("{label}")')
             lines.append(f"style {record_id} fill:#bbf")
 
-        for people, relationship in [
-            (m.authors, "has author"),
-            (m.reviewers, "has reviewer"),
-            (m.creators, "has creator"),
-        ]:
-            for person in people or []:
-                if person.curie not in seen:
-                    seen.add(person.curie)
-                    lines.append(f"{person.curie}[{_n(person)}]")
-                    lines.append(f"style {person.curie} fill:#bef")
-                edges.append((record_id, relationship, person.curie))
+        if include_people:
+            for people, relationship in [
+                (m.authors, "has author"),
+                (m.reviewers, "has reviewer"),
+                (m.creators, "has creator"),
+            ]:
+                for person in people or []:
+                    if person.curie not in seen:
+                        seen.add(person.curie)
+                        lines.append(f"{person.curie}[{_n(person)}]")
+                        lines.append(f"style {person.curie} fill:#bef")
+                    edges.append((record_id, relationship, person.curie))
 
-        if m.source:
+        if include_sources and m.source is not None:
             if m.source.curie not in seen:
                 seen.add(m.source.curie)
                 lines.append(f"{m.source.curie}[{_n(m.source)}]")
@@ -122,6 +141,12 @@ def to_mermaid(mappings: Iterable[SemanticMapping], converter: curies.Converter)
     # TODO prune mappings with no incoming relationships
 
     return "flowchart LR\n" + "\n".join("  " + line for line in lines)
+
+
+def _n(r: curies.Reference) -> str:
+    if name := getattr(r, "name", None):
+        return f"{name}\n{r.curie}"
+    return r.curie
 
 
 def _clean_msi(s: str) -> str:
