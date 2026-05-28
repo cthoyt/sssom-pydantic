38917ffcd03401af9d3059eb67b4c89912a2ed38
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-05-28 09:56:12 +0200

    Cleanup

--- src/sssom_pydantic/evaluation.py
+++ src/sssom_pydantic/evaluation.py
@@ -4,20 +4,26 @@ import itertools as itt
 import logging
 from collections import defaultdict
 from collections.abc import Iterable
+from typing import NamedTuple, TypeAlias, TypeVar
 
 import click
-
-from tqdm import tqdm
+import curies
+from curies.vocabulary import exact_match, lexical_matching_process, manual_mapping_curation
 from ssslm import GildaGrounder, Grounder, LiteralMapping
+from tqdm import tqdm
 
-from curies import Reference
-from sssom_pydantic import SemanticMapping, MappingSet
-from curies.vocabulary import exact_match as EXACT_MATCH, lexical_matching_process as LEXICAL_MATCH
-from curies.vocabulary import lexical_matching_process
+import sssom_pydantic
+from sssom_pydantic import MappingSet, SemanticMapping
+from sssom_pydantic.process import invert_by_prefix_pair
+
 logger = logging.getLogger(__name__)
 
+X = TypeVar("X")
 
-def _get_v1(positive_set, negative_set, predicted_set):
+
+def _get_v1(
+    positive_set: set[X], negative_set: set[X], predicted_set: set[X]
+) -> tuple[int, int, int, int]:
     tp = len(positive_set.intersection(predicted_set))  # true positives
     fp = len(negative_set.intersection(predicted_set))  # false positives
     fn = len(positive_set - predicted_set)  # false negatives
@@ -25,21 +31,48 @@ def _get_v1(positive_set, negative_set, predicted_set):
     return tp, fp, fn, tn
 
 
-def evaluate_predictions(
-    *,
-    positive: Iterable[SemanticMapping],
-    negative: Iterable[SemanticMapping],
-    predicted: Iterable[SemanticMapping],
-    tag: str | None = None,
-):
-    """Evaluate predicted mappings using ground truth positive and negative mappings."""
-    positive_index = get_index(positive, progress=False)
-    negative_index = get_index(negative, progress=False)
-    predicted_index = get_index(predicted, progress=False)
+DD: TypeAlias = dict[str, set[SemanticMapping]]
 
-    positive_set = set(positive_index)
-    negative_set = set(negative_index)
-    predicted_set = set(predicted_index)
+
+def stratify(mappings: Iterable[SemanticMapping], converter: curies.Converter) -> tuple[DD, DD, DD]:
+    """Stratify a set of mappings."""
+    positive, negative, predicted = defaultdict(set), defaultdict(set), defaultdict(set)
+    for mapping in mappings:
+        hsh = converter.hash_triple(mapping)
+        if mapping.justification == lexical_matching_process:
+            predicted[hsh].add(mapping)
+        elif (
+            mapping.justification == manual_mapping_curation and mapping.predicate_modifier is None
+        ):
+            positive[hsh].add(mapping)
+        elif (
+            mapping.justification == lexical_matching_process
+            and mapping.predicate_modifier is not None
+        ):
+            negative[hsh].add(mapping)
+        else:
+            pass  # TODO what to do with others?
+    return dict(positive), dict(negative), dict(predicted)
+
+
+class Evaluation(NamedTuple):
+    """An evaluation tuple."""
+
+    completion: float
+    accuracy: float
+    precision: float
+    recall: float
+    f1: float
+
+
+def evaluate_predictions(
+    mappings: Iterable[SemanticMapping],
+    converter: curies.Converter,
+    *,
+    tag: str | None = None,
+) -> Evaluation:
+    """Evaluate predicted mappings using ground truth positive and negative mappings."""
+    positive_set, negative_set, predicted_set = map(set, stratify(mappings, converter))
 
     tp, fp, fn, tn = _get_v1(positive_set, negative_set, predicted_set)
 
@@ -58,8 +91,9 @@ def evaluate_predictions(
     completion = 1 - predicted_only / len(predicted_set)
 
     # what is the percentage of curated examples that are positive?
-    # positive_percentage = len(positive_set) / (len(positive_set) + len(negative_set))
-    return completion, accuracy, precision, recall, f1
+    _positive_percentage = len(positive_set) / (len(positive_set) + len(negative_set))
+
+    return Evaluation(completion, accuracy, precision, recall, f1)
 
 
 def _get_text_to_literal_mappings(grounder: Grounder) -> dict[str, list[LiteralMapping]]:
@@ -72,9 +106,7 @@ def _get_text_to_literal_mappings(grounder: Grounder) -> dict[str, list[LiteralM
     return dict(dd)
 
 
-def _grounder_to_mappings(
-    grounders: dict[str, Grounder],
-) -> Iterable[tuple[Reference, Reference, float]]:
+def _grounder_to_mappings(grounders: dict[str, Grounder]) -> Iterable[SemanticMapping]:
     terms: dict[str, dict[str, list[LiteralMapping]]] = {
         prefix: _get_text_to_literal_mappings(grounder)
         for prefix, grounder in tqdm(grounders.items(), desc="Indexing texts")
@@ -89,50 +121,32 @@ def _grounder_to_mappings(
             scored_matches = g1.get_matches(text)
             # there are lots of ways to do this, now we do all-by-all
             for literal_mapping, scored_match in itt.product(literal_mappings, scored_matches):
-                yield literal_mapping.reference, scored_match.reference, scored_match.score
-
-
-#: A default confidence for predicated lexical mappings
-LEXICAL_MAPPING_CONFIDENCE = 0.9
-
-
-def grounder_to_mappings(grounders: dict[str, Grounder]) -> list[SemanticMapping]:
-    """Get semantic mappings between a set of grounders."""
-    prefix_list_str = ", ".join(sorted(grounders))
-    mapping_set = MappingSet(
-        name=f"Predicted lexical mappings for {prefix_list_str}",
-        confidence=LEXICAL_MAPPING_CONFIDENCE,
-    )
-    mappings = [
-        SemanticMapping(subject=s, predicate=EXACT_MATCH, objecto=o, justification=lexical_matching_process, confidence=confidence)
-        for s, o, confidence in _grounder_to_mappings(grounders)
-    ]
-    mappings = assemble_evidences(mappings, progress=False)
-    return mapping_set, mappings
+                yield SemanticMapping(
+                    subject=literal_mapping.reference,
+                    predicate=exact_match,
+                    object=scored_match.reference,
+                    justification=lexical_matching_process,
+                    confidence=scored_match.score,
+                )
 
 
 @click.command()
 def main() -> None:
     """Run the workflow for evaluating predicted mappings."""
+    import biomappings
+    import bioregistry
     import pyobo
     import pystow
+    from curies.triples import keep_prefixes_both
     from tabulate import tabulate
 
-    from semra.api import infer_reversible, keep_prefixes
-    from semra.io import from_sssom, write_sssom
-    from semra.sources import (
-        from_biomappings_negative,
-        get_biomappings_positive_mappings,
-        get_clo_mappings,
-    )
+    converter = bioregistry.get_preferred_converter()
 
-    positive_mappings = get_biomappings_positive_mappings()
-    positive_mappings = infer_reversible(positive_mappings, progress=False)
-    click.echo(f"Got {len(positive_mappings):,} positive mappings")
+    positive_biomappings_mappings = biomappings.load_positive_mappings()
+    click.echo(f"Got {len(positive_biomappings_mappings):,} positive mappings from Biomappings")
 
-    negative_mappings = from_biomappings_negative()
-    negative_mappings = infer_reversible(negative_mappings, progress=False)
-    click.echo(f"Got {len(negative_mappings):,} negative mappings")
+    negative_biomappings_mappings = biomappings.load_false_mappings()
+    click.echo(f"Got {len(negative_biomappings_mappings):,} negative mappings from Biomappings")
 
     rows = []
     mesh_grounder = pyobo.get_grounder("mesh")
@@ -140,36 +154,31 @@ def main() -> None:
         path = pystow.join(
             "semra", "evaluation_prediction", name=f"evaluation_prediction_sample_{prefix}.tsv"
         )
-
         if path.is_file():
-            predicted_mappings = from_sssom(path, mapping_set_name=" predictions")
+            predicted_mappings = sssom_pydantic.read(path).mappings
         else:
             grounders = {"mesh": mesh_grounder, prefix: pyobo.get_grounder(prefix)}
-            predicted_mappings = grounder_to_mappings(grounders)
+            predicted_mappings = list(_grounder_to_mappings(grounders))
             click.echo(f"Got {len(predicted_mappings):,} predicted mappings")
-            predicted_mappings = infer_reversible(predicted_mappings, progress=False)
-            write_sssom(predicted_mappings, path)
+            sssom_pydantic.write(
+                predicted_mappings,
+                path,
+                metadata=MappingSet(id=f"https://example.org/{prefix}.tsv"),
+            )
 
-        if prefix == "clo":
-            ontology_mappings = get_clo_mappings()
-            ontology_mappings = keep_prefixes(ontology_mappings, [prefix, "mesh"], progress=False)
-        else:
-            ontology_mappings = from_pyobo(prefix, "mesh")
-        ontology_mappings = infer_reversible(ontology_mappings, progress=False)
+        ontology_mappings = pyobo.get_semantic_mappings(prefix)
         click.echo(f"[{prefix}] got {len(ontology_mappings):,} mappings from the ontology")
 
-        positive_mappings_subset = keep_prefixes(
-            positive_mappings, [prefix, "mesh"], progress=False
+        mappings: Iterable[SemanticMapping] = itt.chain(
+            positive_biomappings_mappings,
+            negative_biomappings_mappings,
+            predicted_mappings,
+            ontology_mappings,
         )
-        negative_mappings_subset = keep_prefixes(
-            negative_mappings, [prefix, "mesh"], progress=False
-        )
-        evaluation_row = evaluate_predictions(
-            positive=itt.chain(positive_mappings_subset, ontology_mappings),
-            negative=negative_mappings_subset,
-            predicted=predicted_mappings,
-            tag=prefix,
-        )
+        mappings = keep_prefixes_both(mappings, [prefix, "mesh"])
+        mappings = invert_by_prefix_pair(mappings, prefix, "mesh", converter=converter)
+
+        evaluation_row = evaluate_predictions(mappings, tag=prefix, converter=converter)
         rows.append((f"[{prefix}](https://bioregistry.io/{prefix})", *evaluation_row))
 
     click.echo(
