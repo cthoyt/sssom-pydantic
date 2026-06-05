e91dbf716f9663490cbfcbe425723c96f7b16290
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-06-05 15:25:05 +0200

    Update impl

--- src/sssom_pydantic/compare.py
+++ src/sssom_pydantic/compare.py
@@ -10,20 +10,23 @@
     $ python -m sssom_pydantic.compare \
         https://github.com/NFDI4Chem/rsc-cmo/raw/refs/heads/Add-tsv-files/src/mappings/fix-mappings.sssom.tsv \
         nfdi-chmo-fix.sssom.tsv \
-        --left-label Ambika
+        --left-label Ambika \
         --right-label Charlie
 """  # noqa:E501
 
 import io
 from collections import defaultdict
+from collections.abc import Collection, Iterable
 from pathlib import Path
 from textwrap import dedent
-from typing import TYPE_CHECKING, Literal
+from typing import TYPE_CHECKING, Any, Generic, Literal, NamedTuple, TypeAlias, TypeVar
 
 import click
+from curies import NamableReference
 from curies.triples import keep_prefixes_both
 from curies.vocabulary import manual_mapping_curation
 from tabulate import tabulate
+from typing_extensions import Self
 
 import sssom_pydantic
 from sssom_pydantic import SemanticMapping
@@ -33,6 +36,52 @@ if TYPE_CHECKING:
     import matplotlib.figure
 
 __all__ = ["compare"]
+
+X = TypeVar("X")
+Y = TypeVar("Y")
+NestedIndex: TypeAlias = defaultdict[X, defaultdict[X, list[Y]]]
+PairIndex: TypeAlias = defaultdict[tuple[X, X], list[Y]]
+
+ZZ = ["subject_id", "subject_label", "object_id", "object_label"]
+
+
+def _zz(k: tuple[NamableReference, NamableReference]) -> tuple[str, str, str, str]:
+    return k[0].curie, k[0].name or "", k[1].curie, k[1].name or ""
+
+
+class VennSets(NamedTuple, Generic[X]):
+    """Represents the elements unique to a left and right set, and their shared elements."""
+
+    left: set[X]
+    both: set[X]
+    right: set[X]
+
+    @classmethod
+    def from_collections(cls, left: Collection[X], right: Collection[X]) -> Self:
+        """Construct from two collections."""
+        left = set(left)
+        right = set(right)
+        return cls(left - right, left & right, right - left)
+
+
+def _get_nested_index_venns(
+    left: NestedIndex[X, Any], right: NestedIndex[X, Any]
+) -> Iterable[tuple[X, VennSets[X]]]:
+    for element in set(right).intersection(left):
+        yield element, VennSets.from_collections(left[element], right[element])
+
+
+class Venn(NamedTuple):
+    """A representation of the counts in a venn diagram."""
+
+    left: int
+    both: int
+    right: int
+
+
+def _venn_counts(ll: Collection[X], rr: Collection[X]) -> Venn:
+    left, both, right = VennSets.from_collections(ll, rr)
+    return Venn(len(left), len(both), len(right))
 
 
 def compare(
@@ -49,104 +98,150 @@ def compare(
     :param left_label: The label for the left mapping source
     :param right_label: The label for the right mapping source
     :param venn_type: The mechanism for producing a venn diagram
+
+    .. warning:: This function assumes that the mappings are only from a
+        one prefix to another prefix
     """
-    if venn_type is None:
-        venn_type = "mermaid"
     left_mappings = [m for m in left_mappings if m.justification == manual_mapping_curation]
     right_mappings = [m for m in right_mappings if m.justification == manual_mapping_curation]
 
-    left_dd = defaultdict(list)
-    right_dd = defaultdict(list)
-    for mapping in left_mappings:
-        left_dd[mapping.subject, mapping.object].append(mapping)
-    for _k, values in left_dd.items():
-        if len(values) > 1:
-            pass
-    left_d = {so: values[0] for so, values in left_dd.items() if len(values) == 1}
+    left_subject_prefixes = {m.subject.prefix for m in left_mappings}
+    left_object_prefixes = {m.object.prefix for m in left_mappings}
+    right_subject_prefixes = {m.subject.prefix for m in right_mappings}
+    right_object_prefixes = {m.object.prefix for m in right_mappings}
+    if len(left_subject_prefixes) != 1:
+        raise ValueError
+    if len(left_object_prefixes) != 1:
+        raise ValueError
+    if left_subject_prefixes != right_subject_prefixes:
+        raise ValueError
+    if left_object_prefixes != right_object_prefixes:
+        raise ValueError
 
+    rv = ""
+
+    left_subject_index: NestedIndex[NamableReference, SemanticMapping] = defaultdict(
+        lambda: defaultdict(list)
+    )
+    right_subject_index: NestedIndex[NamableReference, SemanticMapping] = defaultdict(
+        lambda: defaultdict(list)
+    )
+    left_object_index: NestedIndex[NamableReference, SemanticMapping] = defaultdict(
+        lambda: defaultdict(list)
+    )
+    right_object_index: NestedIndex[NamableReference, SemanticMapping] = defaultdict(
+        lambda: defaultdict(list)
+    )
+    left_dd: PairIndex[NamableReference, SemanticMapping] = defaultdict(list)
+    right_dd: PairIndex[NamableReference, SemanticMapping] = defaultdict(list)
+
+    for mapping in left_mappings:
+        left_subject_index[mapping.subject][mapping.object].append(mapping)
+        left_object_index[mapping.object][mapping.subject].append(mapping)
+        left_dd[mapping.subject, mapping.object].append(mapping)
     for mapping in right_mappings:
+        right_subject_index[mapping.subject][mapping.object].append(mapping)
+        right_object_index[mapping.object][mapping.subject].append(mapping)
         right_dd[mapping.subject, mapping.object].append(mapping)
-    for _k, values in right_dd.items():
+
+    duplicates = []
+    for k, values in left_dd.items():
         if len(values) > 1:
-            pass
+            duplicates.append((left_label, *_zz(k)))
+    for k, values in right_dd.items():
+        if len(values) > 1:
+            duplicates.append((right_label, *_zz(k)))
+
+    if duplicates:
+        rv += "\n\n## Duplicates\n\n"
+        rv += tabulate(duplicates, headers=["side", *ZZ], tablefmt="github")
+        rv += "\n\n"
+
+    left_d = {so: values[0] for so, values in left_dd.items() if len(values) == 1}
     right_d = {so: values[0] for so, values in right_dd.items() if len(values) == 1}
 
+    def _yyyy(x: set[NamableReference]) -> str:
+        return ", ".join(sorted(f"{a.curie} ({a.name})" for a in x))
+
+    subject_rows = []
     # TODO check when entity in left set is mapped to different entity in right set
-
-    left_only = set(left_d) - set(right_d)
-    right_only = set(right_d) - set(left_d)
-    both = set(right_d).intersection(left_d)
-
-    rv = "## Comparison\n\n"
-    rv += f"- {len(left_only)} subject-object pairs {left_label} only\n"
-    rv += f"- {len(right_only)} subject-object pairs {right_label} only\n"
-    rv += f"- {len(both)} subject-object pairs both\n"
-
-    rv += "\n\n"
-    if venn_type == "svg":
-        rv += get_matplotlib_venn2(
-            left=len(left_only),
-            right=len(right_only),
-            both=len(both),
-            left_label=left_label,
-            right_label=right_label,
+    for subject, (l_only, b_both, r_only) in _get_nested_index_venns(
+        left_subject_index, right_subject_index
+    ):
+        if l_only or r_only:
+            subject_rows.append(
+                (subject.curie, subject.name, _yyyy(l_only), _yyyy(b_both), _yyyy(r_only))
+            )
+    if subject_rows:
+        subject_venn = VennSets.from_collections(left_subject_index, right_subject_index)
+        rv += "\n\n## Subject Comparison\n\n"
+        rv += f"- {len(subject_venn.left)} subjects in {left_label} only\n"
+        rv += f"- {len(subject_venn.right)} subject in {right_label} only\n"
+        rv += f"- {len(subject_venn.both)} subjects in both\n\n"
+        rv += tabulate(
+            subject_rows,
+            headers=["subject_id", "subject_label", left_label, "both", right_label],
+            tablefmt="github",
         )
+        rv += "\n\n"
+
+    object_rows = []
+    for obj, (l_only, b_both, r_only) in _get_nested_index_venns(
+        left_object_index, right_object_index
+    ):
+        if l_only or r_only:
+            object_rows.append((obj.curie, obj.name, _yyyy(l_only), _yyyy(b_both), _yyyy(r_only)))
+    if object_rows:
+        object_venn = VennSets.from_collections(left_object_index, right_object_index)
+        rv += "\n\n## Object Comparison\n\n"
+        rv += f"- {len(object_venn.left)} objects in {left_label} only\n"
+        rv += f"- {len(object_venn.right)} objects in {right_label} only\n"
+        rv += f"- {len(object_venn.both)} objects in both\n\n"
+        rv += tabulate(
+            object_rows,
+            headers=["object_id", "object_label", left_label, "both", right_label],
+            tablefmt="github",
+        )
+        rv += "\n\n"
+
+    venn = VennSets.from_collections(left_d, right_d)
+
+    rv += "\n\n## Subject-Object Pair Comparison\n\n"
+    rv += f"- {len(venn.left)} subject-object pairs {left_label} only\n"
+    rv += f"- {len(venn.right)} subject-object pairs {right_label} only\n"
+    rv += f"- {len(venn.both)} subject-object pairs both\n"
+
+    if venn_type is None:
+        pass
+    elif venn_type == "svg":
+        rv += "\n\n"
+        rv += get_matplotlib_venn2(venn, left_label=left_label, right_label=right_label)
+        rv += "\n\n"
     elif venn_type == "mermaid":
-        rv += get_mermaid_venn2_markdown(
-            left=len(left_only),
-            right=len(right_only),
-            both=len(both),
-            left_label=left_label,
-            right_label=right_label,
-        )
-    else:
-        raise ValueError(f"Unknown venn_type: {venn_type}")
-    rv += "\n\n"
+        rv += "\n\n"
+        rv += get_mermaid_venn2_markdown(venn, left_label=left_label, right_label=right_label)
+        rv += "\n\n"
 
-    rows = []
+    subject_object_rows = []
 
-    for k in both:
+    for k in venn.both:
         left = left_d[k]
         right = right_d[k]
         if left.predicate != right.predicate:
-            rows.append(
-                (
-                    k[0].curie,
-                    k[0].name,
-                    k[1].curie,
-                    k[1].name,
-                    "different predicate",
-                    left.predicate.curie,
-                    right.predicate.curie,
-                )
-            )
+            msg = "different predicate"
+            subject_object_rows.append((*_zz(k), msg, left.predicate.curie, right.predicate.curie))
         elif left.predicate_modifier != right.predicate_modifier:
-            rows.append(
-                (
-                    k[0].curie,
-                    k[0].name,
-                    k[1].curie,
-                    k[1].name,
-                    "different predicate modifier",
-                    left.predicate_modifier or "",
-                    right.predicate_modifier or "",
-                )
+            msg = "different predicate modifier"
+            subject_object_rows.append(
+                (*_zz(k), msg, left.predicate_modifier or "", right.predicate_modifier or "")
             )
 
-    if both:
-        rv += "\n\n## Differences\n\n"
+    if subject_object_rows:
+        rv += "\n\n"
         rv += (
             tabulate(
-                rows,
-                headers=[
-                    "subject_id",
-                    "subject_label",
-                    "object_id",
-                    "object_label",
-                    "warning",
-                    left_label,
-                    right_label,
-                ],
+                subject_object_rows,
+                headers=[*ZZ, "warning", left_label, right_label],
                 tablefmt="github",
             )
             + "\n"
@@ -155,44 +250,47 @@ def compare(
 
 
 def get_mermaid_venn2_markdown(
-    left: int, right: int, both: int, left_label: str | None = None, right_label: str | None = None
+    venn: Venn | VennSets[Any], left_label: str | None = None, right_label: str | None = None
 ) -> str:
     """Get a mermaid venn diagram."""
-    return dedent(f"""\
-    ```mermaid
-    {get_mermaid_venn2(left, right, both, left_label=left_label, right_label=right_label)}
-    ```
-    """)
+    rv = "```mermaid\n"
+    rv += get_mermaid_venn2(venn, left_label=left_label, right_label=right_label)
+    rv += "```"
+    return rv
 
 
 def get_mermaid_venn2(
-    left: int, right: int, both: int, left_label: str | None = None, right_label: str | None = None
+    venn: VennSets[Any] | Venn, left_label: str | None = None, right_label: str | None = None
 ) -> str:
     """Get a mermaid venn diagram."""
     if left_label is None:
         left_label = "left"
     if right_label is None:
         right_label = "right"
+    if isinstance(venn, VennSets):
+        venn = Venn(len(venn.left), len(venn.both), len(venn.right))
     return dedent(f"""\
         venn-beta
-          set A["{left_label}"]:{left}
-          set B["{right_label}"]:{right}
-          union A,B["Overlap"]:{both}
+          set A["{left_label}"]:{venn.left}
+          set B["{right_label}"]:{venn.right}
+          union A,B["Overlap"]:{venn.both}
     """)
 
 
 def get_matplotlib_venn2(
-    left: int, right: int, both: int, left_label: str | None = None, right_label: str | None = None
+    venn: VennSets[Any] | Venn, left_label: str | None = None, right_label: str | None = None
 ) -> str:
     """Get SVG from matplotlib."""
     import matplotlib.pyplot as plt
     from matplotlib_venn import venn2
 
+    if isinstance(venn, Venn):
+        subsets = venn.left, venn.right, venn.both
+    else:
+        subsets = len(venn.left), len(venn.right), len(venn.both)
+
     fig = plt.figure(figsize=(4, 2.5))
-    venn2(
-        subsets=(left, right, both),
-        set_labels=(left_label, right_label),
-    )
+    venn2(subsets=subsets, set_labels=(left_label, right_label))
     return fig_to_markdown_svg(fig)
 
 
@@ -226,18 +324,20 @@ def _do_it(o: str | Path, p: Path, target_prefix: str) -> tuple[list[SemanticMap
     return mappings, metadata.title
 
 
+DEMO_RIGHT = "https://github.com/nfdi-de/section-metadata-wg-onto/raw/refs/heads/main/sssom/data/positive.sssom.tsv"
+DEMO_RIGHT_NAME = "Charlie"
+DEMO_LEFT = "https://github.com/NFDI4Chem/rsc-cmo/raw/refs/heads/Add-tsv-files/src/mappings/fix-mappings.sssom.tsv"
+DEMO_LEFT_NAME = "Ambika"
+
+
 @click.command()
-@click.argument("left_url")
-@click.argument("right_url")
-@click.option("--left-label")
-@click.option("--right-label")
+@click.option("--left-url", default=DEMO_LEFT)
+@click.option("--right-url", default=DEMO_RIGHT)
+@click.option("--left-label", default=DEMO_LEFT_NAME)
+@click.option("--right-label", default=DEMO_RIGHT_NAME)
 @click.option("--output", type=Path)
 def _demo(
-    left_url: str,
-    right_url: str,
-    left_label: str | None,
-    right_label: str | None,
-    output: Path | None,
+    left_url: str, right_url: str, left_label: str, right_label: str, output: Path | None
 ) -> None:
     import sys
 
@@ -257,6 +357,9 @@ def _demo(
         left_label or internal_title or "left",
         right_label or external_title or "right",
     )
+    import pyperclip
+
+    pyperclip.copy(markdown)
     pystow.utils.safe_write_text(markdown, output or sys.stdout)
 
 
