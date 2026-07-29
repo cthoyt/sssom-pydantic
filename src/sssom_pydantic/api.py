032586cf1150917527ae16d15ec3f58bdd445058
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-07-29 14:22:50 +0200

    Format docstrs

--- src/sssom_pydantic/api.py
+++ src/sssom_pydantic/api.py
@@ -688,6 +688,7 @@ def row_to_record(
 
     :param row: The raw row dictionary
     :param propagatable: elements that should be propagated to all rows
+
     :returns: A record object
     """
     # Step 1: propagate values from the header if it's not explicit in the record
