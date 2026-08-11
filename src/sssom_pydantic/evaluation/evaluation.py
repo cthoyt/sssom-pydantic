b8622a2063926cc852ac5cc8d3c649623c077740
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-08-11 12:50:23 +0200

    Up

--- src/sssom_pydantic/evaluation/evaluation.py
+++ src/sssom_pydantic/evaluation/evaluation.py
@@ -1,16 +1,15 @@
 """A workflow for evaluating predicted mappings."""
 
-import logging
 from collections import defaultdict
 from collections.abc import Iterable
 from typing import NamedTuple, TypeAlias, TypeVar
 
 import curies
-from curies.vocabulary import lexical_matching_process, manual_mapping_curation
+from curies import Reference
+from curies import vocabulary as v
+from tqdm import tqdm
 
 from sssom_pydantic import SemanticMapping
-
-logger = logging.getLogger(__name__)
 
 X = TypeVar("X")
 
@@ -19,33 +18,51 @@ def _get_v1(
     positive_set: set[X], negative_set: set[X], predicted_set: set[X]
 ) -> tuple[int, int, int, int]:
     tp = len(positive_set.intersection(predicted_set))  # true positives
-    fp = len(negative_set.intersection(predicted_set))  # false positives
     fn = len(positive_set - predicted_set)  # false negatives
+    fp = len(negative_set.intersection(predicted_set))  # false positives
     tn = len(negative_set - predicted_set)  # true negatives
     return tp, fp, fn, tn
 
 
-DD: TypeAlias = dict[str, set[SemanticMapping]]
+UnorderedPrefixPair: TypeAlias = frozenset[str]
+
+DD: TypeAlias = dict[UnorderedPrefixPair, set[str]]
+
+PREDICTION_PREDICATES = {
+    v.lexical_matching_process,
+    v.lexical_similarity_threshold_based_matching_process,
+    v.logical_reasoning_matching_process,
+    v.semantic_similarity,
+    v.structural_matching,
+}
 
 
 def stratify(mappings: Iterable[SemanticMapping], converter: curies.Converter) -> tuple[DD, DD, DD]:
     """Stratify a set of mappings."""
-    positive, negative, predicted = defaultdict(set), defaultdict(set), defaultdict(set)
-    for mapping in mappings:
-        hsh = converter.hash_triple(mapping)
-        if mapping.justification == lexical_matching_process:
-            predicted[hsh].add(mapping)
-        elif (
-            mapping.justification == manual_mapping_curation and mapping.predicate_modifier is None
-        ):
-            positive[hsh].add(mapping)
-        elif (
-            mapping.justification == lexical_matching_process
-            and mapping.predicate_modifier is not None
-        ):
-            negative[hsh].add(mapping)
-        else:
-            pass  # TODO what to do with others?
+    positive: defaultdict[UnorderedPrefixPair, set[str]] = defaultdict(set)
+    negative: defaultdict[UnorderedPrefixPair, set[str]] = defaultdict(set)
+    predicted: defaultdict[UnorderedPrefixPair, set[str]] = defaultdict(set)
+    unhandled_justifications: set[Reference] = set()
+
+    # TODO should predicate be accounted for, or should this workflow
+    #  just assume squashing has been done correctly first?
+    # TODO use broad match and exact match to infer NOT exact match?
+    # TODO squash higher relations into exact match?
+    for m in mappings:
+        # todo could also use hash triple
+        xx = f"{m.subject.curie}-{m.object.curie}"
+        prefix_pair: UnorderedPrefixPair = frozenset([m.subject.prefix, m.object.prefix])
+        if m.justification in PREDICTION_PREDICATES:
+            # assume there are no negative predictions
+            predicted[prefix_pair].add(xx)
+        elif m.justification == v.manual_mapping_curation:
+            if m.predicate_modifier is None:
+                positive[prefix_pair].add(xx)
+            else:
+                negative[prefix_pair].add(xx)
+        elif m.justification not in unhandled_justifications:
+            unhandled_justifications.add(m.justification)
+            tqdm.write(f"unhandled mapping justification: {m.justification.curie}")
     return dict(positive), dict(negative), dict(predicted)
 
 
@@ -64,10 +81,25 @@ def evaluate_predictions(
     converter: curies.Converter,
     *,
     tag: str | None = None,
-) -> Evaluation:
-    """Evaluate predicted mappings using ground truth positive and negative mappings."""
-    positive_set, negative_set, predicted_set = map(set, stratify(mappings, converter))
+) -> dict[UnorderedPrefixPair, Evaluation]:
+    """Evaluate predicted mappings using manually curated positive and negative mappings."""
+    positive_set, negative_set, predicted_set = stratify(mappings, converter)
+    keys = set(positive_set).union(negative_set).union(predicted_set)
+    rv = {}
+    for key in keys:
+        rv[key] = _evaluate_helper(
+            positive_set[key], negative_set[key], predicted_set[key], tag=tag
+        )
+    return rv
 
+
+def _evaluate_helper(
+    positive_set: set[str],
+    negative_set: set[str],
+    predicted_set: set[str],
+    *,
+    tag: str | None = None,
+) -> Evaluation:
     tp, fp, fn, tn = _get_v1(positive_set, negative_set, predicted_set)
 
     predicted_only = len(predicted_set - positive_set - negative_set)
@@ -76,7 +108,7 @@ def evaluate_predictions(
     msg = f"union={union_len:,}, intersection={tp:,}, curated={fn:,}, predicted={predicted_only:,}"
     if tag is not None:
         msg = f"[{tag}] {msg}"
-    logger.info(msg)
+    tqdm.write(msg)
 
     accuracy = (tp + tn) / (tp + tn + fp + fn)
     recall = tp / (tp + fn)
--- src/sssom_pydantic/evaluation/lexical_prediction_demo.py
+++ src/sssom_pydantic/evaluation/lexical_prediction_demo.py
@@ -1,16 +1,19 @@
 """Automatically assess a lexical matching scenario."""
 
 import itertools as itt
+import time
 from collections import defaultdict
-from collections.abc import Iterable
+from collections.abc import Collection, Iterable
+from typing import Any, TypeVar
 
 import biomappings
 import bioregistry
 import click
 import pyobo
 import pystow
-from curies.triples import keep_prefixes_both
+from curies.triples import keep_object_prefixes, keep_prefixes_both
 from curies.vocabulary import exact_match, lexical_matching_process
+from humanize import naturaldelta
 from ssslm import GildaGrounder, Grounder, LiteralMapping
 from tabulate import tabulate
 from tqdm import tqdm
@@ -20,18 +23,28 @@ from sssom_pydantic import MappingSet, SemanticMapping
 from sssom_pydantic.evaluation.evaluation import evaluate_predictions
 from sssom_pydantic.process import invert_by_prefix_pair
 
+X = TypeVar("X")
+
+
+def _tqdm_combinations(x: Collection[X], n: int, **kwargs: Any) -> Iterable[tuple[X, X]]:
+    yield from tqdm(itt.combinations(x, n), total=len(x) * (len(x) - 1) // 2, **kwargs)
+
 
 def _grounder_to_mappings(grounders: dict[str, Grounder]) -> Iterable[SemanticMapping]:
     terms: dict[str, dict[str, list[LiteralMapping]]] = {
         prefix: _get_text_to_literal_mappings(grounder)
-        for prefix, grounder in tqdm(grounders.items(), desc="Indexing texts")
+        for prefix, grounder in tqdm(grounders.items(), desc="Indexing texts", leave=False)
     }
-    for (p1, g1), (p2, _g2) in tqdm(
-        itt.combinations(grounders.items(), 2), unit_scale=True, desc="Generating mappings"
+    for (p1, g1), (p2, _g2) in _tqdm_combinations(
+        grounders.items(),
+        2,
+        unit_scale=True,
+        desc="Generating mappings",
+        leave=False,
     ):
         text_to_terms = terms[p2]
         for text, literal_mappings in tqdm(
-            text_to_terms.items(), unit_scale=True, desc=f"{p1}-{p2} lexical"
+            text_to_terms.items(), unit_scale=True, desc=f"{p1}-{p2} lexical", leave=False
         ):
             scored_matches = g1.get_matches(text)
             # there are lots of ways to do this, now we do all-by-all
@@ -41,7 +54,7 @@ def _grounder_to_mappings(grounders: dict[str, Grounder]) -> Iterable[SemanticMa
                     predicate=exact_match,
                     object=scored_match.reference,
                     justification=lexical_matching_process,
-                    confidence=scored_match.score,
+                    confidence=round(scored_match.score, 4),
                 )
 
 
@@ -58,45 +71,113 @@ def _get_text_to_literal_mappings(grounder: Grounder) -> dict[str, list[LiteralM
 @click.command()
 def main() -> None:
     """Run the workflow for evaluating predicted mappings."""
-    converter = bioregistry.get_preferred_converter()
+    converter = bioregistry.get_converter()
 
+    start = time.time()
     positive_biomappings_mappings = biomappings.load_positive_mappings()
-    click.echo(f"Got {len(positive_biomappings_mappings):,} positive mappings from Biomappings")
+    click.echo(
+        f"Got {len(positive_biomappings_mappings):,} positive mappings from Biomappings"
+        f" in {naturaldelta(time.time() - start)}"
+    )
 
+    prefixes = set()
+    for m in positive_biomappings_mappings:
+        if m.subject.prefix == "mesh":
+            prefixes.add(m.object.prefix)
+        elif m.object.prefix == "mesh":
+            prefixes.add(m.subject.prefix)
+
+    # skip a few
+    prefixes -= {"pubchem.compound", "kegg.pathway", "umls", "ncit", "snomedct"}
+    prefixes = {"maxo"}
+
+    start = time.time()
     negative_biomappings_mappings = biomappings.load_false_mappings()
-    click.echo(f"Got {len(negative_biomappings_mappings):,} negative mappings from Biomappings")
+    click.echo(
+        f"Got {len(negative_biomappings_mappings):,} negative mappings from Biomappings"
+        f" in {naturaldelta(time.time() - start)}"
+    )
+
+    start = time.time()
+    click.echo("getting MeSH grounder")
+    mesh_grounder = pyobo.get_grounder("mesh")
+    click.echo(f"Got MeSH grounder in {naturaldelta(time.time() - start)}")
 
     rows = []
-    mesh_grounder = pyobo.get_grounder("mesh")
-    for prefix in sorted(["chebi", "maxo", "cl", "doid", "go", "uberon", "vo", "clo"]):
-        path = pystow.join(
-            "semra", "evaluation_prediction", name=f"evaluation_prediction_sample_{prefix}.tsv"
-        )
-        if path.is_file():
-            predicted_mappings = sssom_pydantic.read(path).mappings
+    module = pystow.module("sssom", "evaluation_prediction")
+
+    it = tqdm(sorted(prefixes))
+    for prefix in it:
+        prefix = converter.standardize_prefix(prefix, strict=True)
+        it.set_description(f"Evaluating {prefix}")
+        path = module.join(name=f"{prefix}-predicted.sssom.tsv")
+        if path.is_file() and False:
+            predicted_mappings, _, _ = sssom_pydantic.read(path)
+            tqdm.write(
+                f"[{prefix}] loaded {len(predicted_mappings):,} predicted mappings from {path}"
+            )
         else:
-            grounders = {"mesh": mesh_grounder, prefix: pyobo.get_grounder(prefix)}
+            try:
+                external_grounder = pyobo.get_grounder(prefix, force=False)
+            except Exception as e:  # noqa:BLE001
+                tqdm.write(click.style(f"[{prefix}] failed to get grounder: {e}"))
+                continue
+            grounders = {"mesh": mesh_grounder, prefix: external_grounder}
             predicted_mappings = list(_grounder_to_mappings(grounders))
-            click.echo(f"Got {len(predicted_mappings):,} predicted mappings")
+            # TODO deduplicate predicted mappings
+            if not predicted_mappings:
+                tqdm.write(click.style(f"[{prefix}] retrieved no mappings to MeSH"))
+                continue
+            tqdm.write(f"[{prefix}] Got {len(predicted_mappings):,} predicted mappings")
             sssom_pydantic.write(
                 predicted_mappings,
                 path,
-                metadata=MappingSet(id=f"https://example.org/{prefix}.tsv"),
+                # TODO there's a better way to get metadata for a given prefix
+                metadata=MappingSet(id=f"https://example.org/{prefix}-predicted.tsv"),
+                converter=converter,
             )
 
-        ontology_mappings = pyobo.get_semantic_mappings(prefix)
-        click.echo(f"[{prefix}] got {len(ontology_mappings):,} mappings from the ontology")
+        path = module.join(name=f"{prefix}-from-ontology.sssom.tsv")
+        if path.is_file():
+            ontology_mappings, _, _ = sssom_pydantic.read(path)
+            tqdm.write(
+                f"[{prefix}] got {len(ontology_mappings):,} cached mappings from "
+                f"the ontology from {path}"
+            )
+        else:
+            ontology_mappings = list(pyobo.get_semantic_mappings(prefix))
+            ontology_mappings = list(keep_object_prefixes(ontology_mappings, "mesh"))
+            tqdm.write(f"[{prefix}] got {len(ontology_mappings):,} mappings from the ontology")
+            sssom_pydantic.write(
+                ontology_mappings,
+                path,
+                # TODO there's a better way to get metadata for a given prefix
+                metadata=MappingSet(id=f"https://example.org/{prefix}-from-ontology.tsv"),
+                converter=converter,
+            )
 
-        mappings: Iterable[SemanticMapping] = itt.chain(
-            positive_biomappings_mappings,
-            negative_biomappings_mappings,
-            predicted_mappings,
-            ontology_mappings,
+        mappings: list[SemanticMapping] = list(
+            itt.chain(
+                positive_biomappings_mappings,
+                negative_biomappings_mappings,
+                predicted_mappings,
+                ontology_mappings,
+            )
         )
-        mappings = keep_prefixes_both(mappings, [prefix, "mesh"])
-        mappings = invert_by_prefix_pair(mappings, prefix, "mesh", converter=converter)
+        tqdm.write(f"[{prefix}] got {len(mappings):,} merged mappings")
+        mappings = list(keep_prefixes_both(mappings, [prefix, "mesh"]))
+        tqdm.write(f"[{prefix}] got {len(mappings):,} merged mappings filtered to MeSH")
+        mappings = list(invert_by_prefix_pair(mappings, prefix, "mesh", converter=converter))
+        sssom_pydantic.write(
+            mappings,
+            module.join(name=f"{prefix}-merged.tsv"),
+            metadata=MappingSet(id=f"https://example.org/{prefix}-merged.tsv"),
+            converter=converter,
+        )
 
-        evaluation_row = evaluate_predictions(mappings, tag=prefix, converter=converter)
+        evaluation_results = evaluate_predictions(mappings, tag=prefix, converter=converter)
+        evaluation_row = next(iter(evaluation_results.values()))
+
         rows.append((f"[{prefix}](https://bioregistry.io/{prefix})", *evaluation_row))
 
     click.echo(
