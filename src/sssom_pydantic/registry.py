4d8aef43fa5b981cc8c0b5ecf4e9b1bbe0e82b71
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-09-16 10:04:31 +0200

    Update registry.py

--- src/sssom_pydantic/registry.py
+++ src/sssom_pydantic/registry.py
@@ -2,12 +2,14 @@
 
 import hashlib
 import logging
+from collections import Counter
 from pathlib import Path
-from typing import Self
+from typing import Annotated, Self
 
 import click
 import pystow
 from pydantic import AnyUrl, BaseModel, ConfigDict, Field
+from pystow.utils import name_from_url
 from pystow.utils.pydantic_utils import read_pydantic_yaml
 from tqdm import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
@@ -30,11 +32,17 @@ SKIPS_REGISTRIES = {
 }
 SKIP_SSSOM = {"http://w3id.org/sssom/commons/monarch/mappings/mondo_hp_lexical.sssom.tsv"}
 
+MODULE = pystow.module("sssom", "mapping-commons")
 
-def _get_path(url: AnyUrl, *, force: bool = False) -> Path:
-    md5 = hashlib.md5(usedforsecurity=False)
-    md5.update(str(url).encode("utf-8"))
-    path = pystow.ensure("tmp", url=str(url), name=md5.hexdigest(), force=force)
+
+def _get_path(url: AnyUrl, *, code: str, name: str | None, force: bool = False) -> Path:
+    url_str = str(url)
+    if name is None:
+        # do this in case of duplicate file names within a given registry
+        md5 = hashlib.md5(usedforsecurity=False)
+        md5.update(url_str.encode("utf-8"))
+        name = md5.hexdigest()[:6] + "-" + name_from_url(url_str)
+    path = MODULE.ensure(code, url=url_str, name=name, force=force)
     return path
 
 
@@ -46,23 +54,24 @@ class MappingSetReference(BaseModel):
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
-    url: AnyUrl = Field(..., alias="mapping_set_id")
-    group: str = Field(..., alias="mapping_set_group")
-    confidence: float | None = Field(None, alias="registry_confidence")
+    url: Annotated[AnyUrl, Field(alias="mapping_set_id")]
+    group: Annotated[str, Field(alias="mapping_set_group")]
+    confidence: Annotated[float | None, Field(alias="registry_confidence")] = None
     local_name: str | None = None
 
-    mapping_set: MappingSet | None = Field(None, exclude=True)
-    mappings: list[SemanticMapping] | None = Field(None, repr=False, exclude=True)
-    errors: list[ParseError] | None = Field(None, repr=False, exclude=True)
+    mapping_set: Annotated[MappingSet | None, Field(exclude=True)] = None
+    mappings: Annotated[list[SemanticMapping] | None, Field(repr=False, exclude=True)] = None
+    errors: Annotated[list[ParseError] | None, Field(None, repr=False, exclude=True)] = None
 
-    def hydrate(self, *, force: bool = False) -> None:
+    def hydrate(self, *, code: str, force: bool = False) -> None:
         """Hydrate the mappings and metadata from this mapping set."""
         if str(self.url) in SKIP_SSSOM:
             return
         try:
             with logging_redirect_tqdm():
+                path = _get_path(self.url, code=code, name=self.local_name, force=force)
                 self.mappings, _converter, self.mapping_set, self.errors = sssom_pydantic.read(
-                    _get_path(self.url, force=force),
+                    path,
                     return_errors=True,
                     progress=True,
                     progress_kwargs={"leave": False},
@@ -71,19 +80,25 @@ class MappingSetReference(BaseModel):
             tqdm.write(click.style(f"\n{self.url} uncaught exception in {e}\n", fg="red"))
         else:
             if self.mappings:
-                tqdm.write(
-                    click.style(f"{self.url} had {len(self.mappings):,} mappings", fg="green")
-                )
+                label = str(self.url)
+                if self.mapping_set is not None and self.mapping_set.title is not None:
+                    label += f" ({self.mapping_set.title})"
+                elif self.local_name:
+                    label += f" ({self.local_name})"
+                tqdm.write(click.style(f"{label} had {len(self.mappings):,} mappings", fg="green"))
             if self.errors:
+                error_path = path.with_suffix(".error.txt")
                 tqdm.write(
                     click.style(
-                        f"{self.url} had {len(self.errors):,} errors. Sampling:", fg="yellow"
+                        f"{self.url} had {len(self.errors):,} errors. See {error_path}", fg="yellow"
                     )
                 )
-                sample = False
-                if sample:
+                with error_path.open(mode="w") as error_file:
                     for error in self.errors[:5]:
-                        tqdm.write(f"Error on {error.line_number}:\n{_get_exc(error.exception)}\n")
+                        tqdm.write(
+                            f"Error on {error.line_number}:\n{_get_exc(error.exception)}\n",
+                            file=error_file,
+                        )
 
 
 class Registry(BaseModel):
@@ -99,15 +114,23 @@ class Registry(BaseModel):
     documentation: AnyUrl | None = None  # weirdly, not part of the SSSOM schema
     mapping_set_references: list[MappingSetReference]
 
-    def hydrate(self, *, force: bool = False) -> None:
+    def hydrate(self, *, code: str, force: bool = False) -> None:
         """Hydrate the mappings and metadata for each mapping set."""
+        counter = Counter(
+            name_from_url(str(mapping_set_ref.url))
+            for mapping_set_ref in self.mapping_set_references
+            if mapping_set_ref.local_name is None
+        )
+        counter = Counter({k: count for k, count in counter.items() if count > 1})
+        if counter:
+            raise ValueError(f"registry has duplicate names w/o using local names: {classmethod}")
         for mapping_set_ref in tqdm(
             self.mapping_set_references,
             desc=f"Hydrating registry {self.iri}",
             unit="mapping set",
             leave=False,
         ):
-            mapping_set_ref.hydrate(force=force)
+            mapping_set_ref.hydrate(code=code, force=force)
 
     # TODO remove description if doubles title
 
@@ -135,7 +158,8 @@ class ServerEntry(BaseModel):
                     click.style(f"{self.url} failed to parse registry\n\n{_get_exc(e)}", fg="red")
                 )
             else:
-                self.registry.hydrate(force=force)
+                #  does code need normalization?
+                self.registry.hydrate(code=self.code, force=force)
 
 
 class Server(BaseModel):
@@ -161,7 +185,8 @@ def get_registry(url: str) -> Registry:
 
 def get_server(url: str) -> Server:
     """Get metadata about a server from a URL."""
-    return read_pydantic_yaml(url, Server)
+    path = MODULE.ensure(url=url, force=True)
+    return read_pydantic_yaml(path, Server)
 
 
 @click.command()
