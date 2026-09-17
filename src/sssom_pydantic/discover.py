9a310cc334849d69990a6082ee877270b589177c
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-09-17 12:42:33 +0200

    Update discover.py

--- src/sssom_pydantic/discover.py
+++ src/sssom_pydantic/discover.py
@@ -1,8 +1,6 @@
-"""
+"""Discover SSSOM from GitHub."""
 
-Discover frm https://github.com/search?q=path%3A*.sssom.tsv%20-is%3Afork&type=code&ref=advsearch&p=5
-
-"""
+import click
 import json
 
 from pystow.github import search_code
@@ -12,15 +10,20 @@ from pystow.utils import write_json
 QUERY = "path:*.sssom.tsv -is:fork"
 
 
-def main():
+@click.command()
+@click.option("--refresh", is_flag=True)
+def main(refresh: bool) -> None:
+    """Discover SSSOM from GitHub."""
+    refresh = True
     path = pystow.join("sssom", name="github-search.json")
-    if not path.is_file():
+    if not path.is_file() or refresh:
         results = list(search_code(QUERY))
         write_json(results, path)
     else:
         results = json.loads(path.read_text())
 
-    print(results[0])
+    click.echo(str(results[0]))
+
 
 if __name__ == '__main__':
     main()
